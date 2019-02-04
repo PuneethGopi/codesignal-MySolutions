b0a1c3c5bb56e67785a2340cf2c9fe81651fa5d5
PuneethGopi <43577019+PuneethGopi@users.noreply.github.com>
2019-02-04 13:31:40 -0600

    Update areEquallyStrong.py

--- areEquallyStrong/areEquallyStrong.py
+++ areEquallyStrong/areEquallyStrong.py
@@ -1,1 +1,9 @@
-adsac
+def areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight):
+    own = sorted([yourLeft, yourRight])
+    friend = sorted([friendsLeft, friendsRight])
+    if (yourLeft+yourRight !=friendsLeft+friendsRight):
+        return False
+    elif(own[0] != friend[0]):
+        return False
+    else:
+        return True
